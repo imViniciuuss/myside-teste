73ac9904fff769ce9722501fc67b9931c4803742
imViniciuuss <imviniciuuss@gmail.com>
2026-04-29 16:28:11 -0300

    chore: scaffold Next.js project with TypeScript and ESLint

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
