62016bf0d7b24ca01d9671da97ec60dd55a70f3a
imViniciuuss <imviniciuuss@gmail.com>
2026-04-29 16:29:07 -0300

    feat: add Product and CartItem type definitions

---
+++ types/index.ts
@@ -0,0 +1,25 @@
+export interface ProductRating {
+  rate: number;
+  count: number;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: ProductRating;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+export type CartAction =
+  | { type: "ADD_ITEM"; product: Product }
+  | { type: "REMOVE_ITEM"; productId: number }
+  | { type: "UPDATE_QUANTITY"; productId: number; quantity: number }
+  | { type: "CLEAR_CART" };
